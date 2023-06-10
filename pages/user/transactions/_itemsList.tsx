ef13d1613c1b060aa16d737292e857c038f53d0e
Beatriz Fernandes <43864137+BeaFernandes@users.noreply.github.com>
2023-06-10 14:37:41 -0300

    Created user transactions page (#31)
    Co-authored-by: Beatriz Pessoal <beatriz-pessoal@pop-os.localdomain>

---
+++ pages/user/transactions/_itemsList.tsx
@@ -0,0 +1,41 @@
+import { Table, Text, UnstyledButton } from '@mantine/core';
+import { Order, Product } from '@prisma/client';
+import moment from 'moment';
+
+export type ComposedOrder = Order & {
+  product: Product,
+}
+
+interface TransactionsPageProps {
+  orders: Array<ComposedOrder>
+}
+
+export default function ItemsList({ orders }: TransactionsPageProps) {
+
+  return (
+    <>
+      <Table highlightOnHover verticalSpacing="sm" c='#343434' striped>
+        <thead>
+          <tr>
+            <th>Data da compra</th>
+            <th>Produto</th>
+            <th>Valor</th>
+            <th>Quantidade</th>
+            <th>Total da compra</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) =>
+            <tr key={order.id}>
+              <td>{moment(order.createdAt).format('lll')}</td>
+              <td>{order.product.name}</td>
+              <td>{`${order.productPrice} Pila`.replace('.', ',')}</td>
+              <td>{order.amount}</td>
+              <td>{`${order.amount*order.productPrice} Pila`.replace('.', ',')}</td>
+            </tr>  
+          )}
+        </tbody>
+      </Table>
+    </>
+  )
+}
